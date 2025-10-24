bc7c19c046d7d78bef45f2c36758de706871aea5
Lhekdup <lhekduptenzin@gmail.com>
2025-10-24 16:14:50 -0400

    screenshare & camera finished

--- MessagingApp/app/src/main/java/ca/gbc/comp3074/myapplication/CameraOffActivity.java
+++ MessagingApp/app/src/main/java/ca/gbc/comp3074/myapplication/CameraOffActivity.java
@@ -1,6 +1,9 @@
 package ca.gbc.comp3074.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CameraOffActivity extends AppCompatActivity {
+
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,17 @@ public class CameraOffActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        backButton = findViewById(R.id.backbutton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CameraOffActivity.this, CallActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
